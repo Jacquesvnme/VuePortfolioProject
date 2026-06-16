19c334e619fc96f4ae8ff205bbc8b2e3f93ead76
Jacques <jacquesvnme@gmail.com>
2026-06-16 18:04:38 +0200

    Added router components
    using ai :(
    Added additional modifications to route to all later added pages

---
+++ src/router/index.js
@@ -0,0 +1,48 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+import Home from '../Pages/Home.vue'
+import PortfolioDashboard from '../Pages/PortfolioDashboard.vue'
+import AboutMe from '../Pages/PortfolioSections/AboutMe.vue'
+import Details from '../Pages/PortfolioSections/Details.vue'
+import Skills from '../Pages/PortfolioSections/Skills.vue'
+import AcademicHistory from '../Pages/PortfolioSections/AcademicHistory.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+  },
+  {
+    path: '/Portfolio-Dashboard',
+    name: 'PortfolioDashboard',
+    component: PortfolioDashboard,
+  },
+  {
+    path: '/Academic-History',
+    name: 'AcademicHistory',
+    component: AcademicHistory,
+  },
+  {
+    path: '/Details',
+    name: 'Details',
+    component: Details,
+  },
+  {
+    path: '/Skills',
+    name: 'Skills',
+    component: Skills,
+  },
+  {
+    path: '/About-Me',
+    name: 'AboutMe',
+    component: AboutMe,
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+export default router
